8d0f8e72797d2c8b1cc5de2cfc19cfa107a60581
Elizabeth <elizabeth.mosley136@gmail.com>
2020-03-04 21:01:52 -0500

    api try 1

---
+++ src/app/api.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  apiUrl = "https://api.data.charitynavigator.org/v2/Categories?app_key=673f09ef0609a336db46bd2cc7630d6e&app_id=0e8799c4";
+
+  constructor(private http: HttpClient) { }
+  getList = () => this.http.get(this.apiUrl);;
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  data = null;
+  constructor(private apiService: ApiService) {}
   title = 'final-project';
+  ngOnInit() {
+  this.data = this.apiService.getList().subscribe((data) => console.log(data));
 }
+}
+
