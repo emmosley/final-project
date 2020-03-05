0c4bd89287dd28d3df8bdd45f926dad356fd4165
Kyle <kyle@kylechase.com>
2020-03-05 18:50:33 -0500

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CardComponent } from './card/card.component';
+import { ListComponent } from './list/list.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CardComponent,
+    ListComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule
