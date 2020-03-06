00c2d0e5ca3d20aaa8f7d46be4cc2cae8ea52ddc
Kyle <kyle@kylechase.com>
2020-03-05 19:16:47 -0500

    router added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CardComponent } from './card/card.component';
 import { ListComponent } from './list/list.component';
 import { DetailsComponent } from './details/details.component';
+
 
 @NgModule({
   declarations: [
@@ -22,5 +25,32 @@ import { DetailsComponent } from './details/details.component';
   ],
   providers: [],
   bootstrap: [AppComponent]
+})
+export class AppModule { }
+
+const appRoutes: Routes = [
+  { path: 'crisis-center', component: CrisisListComponent },
+  { path: 'hero/:id',      component: HeroDetailComponent },
+  {
+    path: 'heroes',
+    component: HeroListComponent,
+    data: { title: 'Heroes List' }
+  },
+  { path: '',
+    redirectTo: '/heroes',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+    // other imports here
+  ],
+  ...
 })
 export class AppModule { }
