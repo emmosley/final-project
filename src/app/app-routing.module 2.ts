07d9950df495a286d25680a2e9ed79c733f78706
Idris Hobdy <idrishobdy@yahoo.com>
2020-03-05 20:04:53 -0500

    routing updated

--- src/app/app-routing.module 2.ts
+++ src/app/app-routing.module 2.ts
@@ -4,7 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { CardComponent } from './card/card.component';
 import { DetailsComponent } from './details/details.component';
 import { ListComponent } from './list/list.component';
-import { AppRoutingModule } from './app-routing.module 2'
+
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },  
@@ -16,7 +16,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes), AppRoutingModule],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
