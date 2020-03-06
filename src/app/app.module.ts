82f52111ceafa27d7036f79472297a16b51dd2d5
Kyle <kyle@kylechase.com>
2020-03-05 20:42:57 -0500

    material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
-
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CardComponent } from './card/card.component';
 import { ListComponent } from './list/list.component';
 import { DetailsComponent } from './details/details.component';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatInputModule } from '@angular/material/input';
 
 
 @NgModule({
@@ -18,10 +19,19 @@ import { DetailsComponent } from './details/details.component';
     CardComponent,
     ListComponent,
     DetailsComponent
+    // MatSliderModule,
+    // RouterModule,
+    
+
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    MatSliderModule,
+    BrowserAnimationsModule,
+    RouterModule,
+    MatInputModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
